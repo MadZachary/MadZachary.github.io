07aaa55e8c8f7c307bd43a63b492b991c99a1aab
MadZachary <90701706+MadZachary@users.noreply.github.com>
2023-09-09 05:53:44 +0300

    aight bye bye

--- testfunnies/swcursor.js
+++
@@ -1,9 +0,0 @@
-//This is just to fix an issue on other browsers
-            var cursorContainer;
-            var removeCursorContainer = setInterval(function() {
-                cursorContainer = document.getElementById('cursor-container');
-                if (cursorContainer) {
-                    cursorContainer.parentNode.removeChild(cursorContainer);
-                    clearInterval(removeCursorContainer);
-                }
-            }, 10); 
